de03a9d8a94e518efd14b69668795fd5bdf9ff4e
DukeSpontaneous <eai_bat@tut.by>
2023-07-06 14:56:06 +0300

    Проверка выдач api/products.

---
+++ test/hermione/api.hermione.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+
+describe("API (e2e):", async function () {
+  it("дополнительно: api/products возвращает корректные данные", async function () {
+    const { data } = await axios.get(
+      "http://localhost:3000/hw/store/api/products"
+    );
+    for (const entry of data) {
+      expect(entry).toHaveProperty("id");
+      expect(entry).toHaveProperty("name");
+      expect(entry).toHaveProperty("price");
+    }
+  });
+
+  for (const id of [0, 1, 2]) {
+    it(`дополнительно: api/products даёт выдачи с id ${id}`, async function () {
+      const { data } = await axios.get(
+        `http://localhost:3000/hw/store/api/products/${id}`
+      );
+
+      expect(data.id).toEqual(id);
+    });
+  }
+});
