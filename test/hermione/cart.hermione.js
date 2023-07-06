7df066ff829cd923c7ea02574652c88d95e90f23
DukeSpontaneous <eai_bat@tut.by>
2023-07-06 04:41:40 +0300

    Проверка меню заказа.

--- test/hermione/cart.hermione.js
+++ test/hermione/cart.hermione.js
@@ -15,4 +15,25 @@ describe("Корзина (e2e):", async function () {
     const [value] = text.match(/\d+/) ?? [""];
     expect(value).toEqual("27");
   });
+
+  it("4-дополнительно: после подтверждения заказа должно быть уведомление об успехе операции", async function () {
+    await this.browser.url("http://localhost:3000/hw/store/catalog/1");
+    const addButton = this.browser.$(".ProductDetails-AddToCart");
+    await addButton.click();
+    await this.browser.url("http://localhost:3000/hw/store/cart");
+
+    const nameInput = this.browser.$("#f-name");
+    const phoneInput = this.browser.$("#f-phone");
+    const addressInput = this.browser.$("#f-address");
+
+    await nameInput.setValue("Имя");
+    await phoneInput.setValue("1234567890");
+    await addressInput.setValue("Адрес");
+
+    const submitButton = this.browser.$(".Form-Submit");
+    await submitButton.click();
+
+    const message = this.browser.$(".Cart-SuccessMessage");
+    expect(await message.isExisting()).toBeTruthy();
+  });
 });
